784000ad90751a2e806c8ee3582a5331bfe577b9
Sumit Kumar Pandey <officialsumitpandey@gmail.com>
2024-11-11 20:21:41 +0530

    cleanup and restructure

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import React from "react";
+import { Button } from "@/components/ui/Button";
 export default function page() {
   return (
     <div>
-      <h1>page</h1>
+      <h1>THis is homepage</h1>
+      <Button variant="outline" size="sm">
+        hello
+      </Button>
     </div>
   );
 }
